4644a5e6f0d4d54ae8327e0c1df6d16af4cb3a29
mikealy <minghan.mike.li@gmail.com>
2020-05-08 14:14:18 -0400

    Initial commit

---
+++ src/main/java/com/github/mikealy/realismdarkness/RealismDarkness.java
@@ -0,0 +1,7 @@
+package com.github.mikealy.realismdarkness;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RealismDarkness.MODID)
+public final class RealismDarkness {
+	public static final String MODID = "realismdarkness";
+}
