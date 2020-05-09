0535c13cf07f85267ae28587b1b7d9329289b2ad
mikealy <minghan.mike.li@gmail.com>
2020-05-09 13:08:14 -0400

    Added dimension whitelist to config settings

--- src/main/java/com/github/mikealy/realismdarkness/config/RealismDarknessConfig.java
+++ src/main/java/com/github/mikealy/realismdarkness/config/RealismDarknessConfig.java
@@ -13,8 +13,21 @@ import com.github.mikealy.realismdarkness.RealismDarkness;
 
 @EventBusSubscriber(modid = RealismDarkness.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class RealismDarknessConfig {
+	// For now, the server can use the client config class since it implements no
+	// separate controls
+	// but the mod is structured so that this can be changed if necessary
+	public static final ClientConfig SERVER;
+	public static final ForgeConfigSpec SERVER_SPEC;
+
+	static {
+		final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+		SERVER_SPEC = specPair.getRight();
+		SERVER = specPair.getLeft();
+	}
+
 	public static final ClientConfig CLIENT;
 	public static final ForgeConfigSpec CLIENT_SPEC;
+
 	static {
 		final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
 		CLIENT_SPEC = specPair.getRight();
@@ -27,21 +40,28 @@ public class RealismDarknessConfig {
 	public static boolean darkTwilightForest;
 	public static boolean alternativeNightSky;
 	public static double gammaOverride;
-	public static double moonlightMultiplier;
-	HashSet<Integer> dimensionBlacklist;
+	public static HashSet<Double> moonlightMultiplierlist;
+	public static HashSet<Integer> dimensionBlacklist;
+	public static HashSet<Integer> dimensionWhitelist;
 
 	@SubscribeEvent
 	public static void onModConfigEvent(final ModConfig.ModConfigEvent configEvent) {
-		if (configEvent.getConfig().getSpec() == RealismDarknessConfig.CLIENT_SPEC) {
-			bakeConfig();
-		}
+		if (configEvent.getConfig().getSpec() == RealismDarknessConfig.CLIENT_SPEC)
+			bakeConfig(CLIENT);
+		else if (configEvent.getConfig().getSpec() == RealismDarknessConfig.SERVER_SPEC)
+			bakeConfig(SERVER);
 	}
 
-	public static void bakeConfig() {
-		mode = CLIENT.mode.get();
-		darkNether = CLIENT.darkNether.get();
-		darkEnd = CLIENT.darkEnd.get();
-		darkTwilightForest = CLIENT.darkTwilightForest.get();
+	public static void bakeConfig(ClientConfig config) {
+		mode = config.mode.get();
+		darkNether = config.darkNether.get();
+		darkEnd = config.darkEnd.get();
+		darkTwilightForest = config.darkTwilightForest.get();
+		alternativeNightSky = config.alternativeNightSky.get();
+		gammaOverride = config.gammaOverride.get();
+		moonlightMultiplierlist = config.moonlightMultiplierlist.get();
+		dimensionBlacklist = config.dimensionBlacklist.get();
+		dimensionWhitelist = config.dimensionWhitelist.get();
 	}
 
 	public static class ClientConfig {
@@ -51,43 +71,38 @@ public class RealismDarknessConfig {
 		public final ForgeConfigSpec.BooleanValue darkTwilightForest;
 		public final ForgeConfigSpec.BooleanValue alternativeNightSky;
 		public final ForgeConfigSpec.DoubleValue gammaOverride;
-		public final ForgeConfigSpec.DoubleValue moonlightMultiplier;
+		public final ForgeConfigSpec.ConfigValue<HashSet<Double>> moonlightMultiplierlist;
 		public final ForgeConfigSpec.ConfigValue<HashSet<Integer>> dimensionBlacklist;
+		public final ForgeConfigSpec.ConfigValue<HashSet<Integer>> dimensionWhitelist;
 
 		public ClientConfig(ForgeConfigSpec.Builder builder) {
-			mode = builder
-					.comment("Display darkness mode: 0: No minimum sky & block light, 1: No minimum block light, 2: Skylight is dependent on moon phase")
-					.translation(RealismDarkness.MOD_ID + ".config." + "mode")
-					.defineInRange("mode", 0, 0, 2);
-			darkNether = builder
-					.comment("Should the Nether have its minimum light removed")
-					.translation(RealismDarkness.MOD_ID + ".config." + "darkNether")
-					.define("darkNether", true);
-			darkEnd = builder
-					.comment("Should the End have its minimum light removed")
-					.translation(RealismDarkness.MOD_ID + ".config." + "darkEnd")
-					.define("darkEnd", false);
-			darkTwilightForest = builder
-					.comment("Should the Twilight Forest have its minimum light removed")
+			mode = builder.comment(
+					"Display darkness mode: 0: No minimum sky & block light, 1: No minimum block light, 2: Skylight is dependent on moon phase")
+					.translation(RealismDarkness.MOD_ID + ".config." + "mode").defineInRange("mode", 0, 0, 2);
+			darkNether = builder.comment("Should the Nether have its minimum light removed")
+					.translation(RealismDarkness.MOD_ID + ".config." + "darkNether").define("darkNether", true);
+			darkEnd = builder.comment("Should the End have its minimum light removed")
+					.translation(RealismDarkness.MOD_ID + ".config." + "darkEnd").define("darkEnd", false);
+			darkTwilightForest = builder.comment("Should the Twilight Forest have its minimum light removed")
 					.translation(RealismDarkness.MOD_ID + ".config." + "darkTwilightForest")
 					.define("darkTwilightForest", true);
 			alternativeNightSky = builder
 					.comment("Should the night sky in modes 1 and 2 be changed to a greener colour")
 					.translation(RealismDarkness.MOD_ID + ".config." + "alternativeNightSky")
 					.define("alternativeNightSky", true);
-			gammaOverride = builder
-					.comment("Lock the sky brightness to some value between 0 and 1, -1 to disable")
+			gammaOverride = builder.comment("Lock the sky brightness to some value between 0 and 1, -1 to disable")
 					.translation(RealismDarkness.MOD_ID + ".config." + "gammaOverride")
 					.defineInRange("gammaOverride", -1D, -1D, 1D);
-			moonlightMultiplier = builder
-					.comment("Set the maximum moon brightness to this multiple of the original brightness between 0 and 1 (Only applicable in mode 2)")
+			moonlightMultiplierlist = builder
+					.comment("Set the maximum moon brightness for each moon phase (Only applicable in mode 2)")
 					.translation(RealismDarkness.MOD_ID + ".config." + "moonlightMultiplier")
-					.defineInRange("moonlightMultiplier", 0.3D, 0D, 1D);
-			dimensionBlacklist = builder
-					.comment("Blacklist dimensions which Hardcore Darkness should not work in")
+					.define("moonlightMultiplierlist", new HashSet<>());
+			dimensionBlacklist = builder.comment("Blacklist dimensions which Hardcore Darkness should not work in(takes precedence over whitelist)")
 					.translation(RealismDarkness.MOD_ID + ".config." + "dimensionBlacklist")
 					.define("dimensionBlacklist", new HashSet<>());
+			dimensionWhitelist = builder.comment("Whitelist dimensions which Hardcore Darkness should work in(overwritten by blacklist if same id appears in both)")
+					.translation(RealismDarkness.MOD_ID + ".config." + "dimensionWhitelist")
+					.define("dimensionWhitelist", new HashSet<>());
 		}
 	}
 }
-
